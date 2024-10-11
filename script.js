6d7765a67d4b8580c65d73b6358d922cc109964a
alexandru233847 <alexutualexino33@gmail.com>
2024-10-11 16:55:23 +0200

    Fixed the error

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Array objects
 const bike = [
-    {name: "bike 1"},
-    {name: "bike 2"},
-    {name: "bike 3"}
+    { name: "bike 1", weight: 7.5 },
+    { name: "bike 2", weight: 6.8 },
+    { name: "bike 3", weight: 8.2 }
 ];
 
 // Light bike
 let light = bike[0]
 
 for (let i= 1; i < bike.length; i++) {
-    if (bike[i].name.length < light.name.length) {
+    if (bike[i].weight < light.weight) {
         light = bike[i]
     }
 }
