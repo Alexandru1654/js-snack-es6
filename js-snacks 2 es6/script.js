ed435f730ef53e8652ebbf701ab3c386f207321c
alexandru233847 <alexutualexino33@gmail.com>
2024-10-11 17:51:36 +0200

    Generate a random numbers, and print in console

--- js-snacks 2 es6/script.js
+++ js-snacks 2 es6/script.js
@@ -4,3 +4,16 @@ const teams = [
     {name: "Inter", points: 0, fouls: 0}
 ];
 
+// Generate a random numbers
+teams.forEach(team => {
+    team.points = Math.floor(Math.random() * 100);
+    team.fouls = Math.floor(Math.random() * 50);
+});
+
+const newArray = teams.map(team => {
+    return { name: team.name, points: team.points, fouls: team.fouls };
+});
+
+//Print 
+console.log(newArray)
+
